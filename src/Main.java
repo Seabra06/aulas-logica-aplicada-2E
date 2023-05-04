0d16b9207a86447a6c7365d4f9dff79397c23c9d
Seabra06 <seabrafabricio30@gmail.com>
2023-05-04 11:55:04 -0300

    Aula de introdução 00

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // laço enquanto- while
-        int cont =0;
-        while (cont <10){
-            System.out.println("Contador = " + cont);
-            //incremento
-            cont = cont + 1;
-        }
-        //laço do para faça - for
-        for(int x = 0; x < 10; x++){
-            System.out.println("Contador = " + x);
-        }
-        // laço faça enquanto - do while
-        int conte = 1;
-        do{
-            //processo
-            System.out.println(cont);
-            conte++; // incremento
-        }while (conte <= 10);
+       // Aula introdutória de Orientação à Objetos
     }
 }
