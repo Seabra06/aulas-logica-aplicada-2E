f16546d1536fd0d5bb6321c480630e4ce6e8320c
Seabra06 <seabrafabricio30@gmail.com>
2023-05-18 12:28:28 -0300

    você pode colocar caracterisca na variavel, como:preço, marca e tamanho

---
+++ src/Pessoa.java
@@ -0,0 +1,11 @@
+import java.util.function.DoubleToIntFunction;
+
+public class Pessoa {
+    // Atributos da pessoa
+    public String nome;
+    public String sobrenome;
+    //Métodos da pessoa
+    public void falar(){
+        System.out.println("falei");
+    }
+}
